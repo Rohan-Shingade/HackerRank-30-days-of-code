68ce279d5f1553411b0b4330de5cdbee08a18605
CarryAllen <rohanshingade1095@gmail.com>
2023-04-27 11:41:13 +0530

    First commit

---
+++ Day 1/Day_1_Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Day_1_Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String word1 = "Hello, World." ;
+        System.out.print("Enter your String : ");
+        String word2 = sc.nextLine();
+        sc.close();
+        
+        System.out.println(word1);
+        System.out.println(word2);
+    }
+}
